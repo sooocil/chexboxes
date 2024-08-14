ebf3a9f57d313983098fe1172f1d25efecbc60f6
Sushil <63572085+sooocil@users.noreply.github.com>
2024-08-14 04:53:16 +0000

    Feat : Added Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter as Router} from 'react-router-dom'
+import ReactGA from 'react-ga4';
+
+ReactGA.initialize('YOUR_MEASUREMENT_ID');
+
+ReactGA.send({ hitType: 'pageview', page: window.location.pathname + window.location.search });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
